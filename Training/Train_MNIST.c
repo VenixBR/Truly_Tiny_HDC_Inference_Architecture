7e08943944460658af39b9fdeeffbb33739a6413
venixbr <viniciusschultz@localhost.localdomain>
2026-09-09 18:30:11 -0300

    Encoding in progress

--- Training/Train_MNIST.c
+++ Training/Train_MNIST.c
@@ -6,9 +6,11 @@
 #define NUM_CLASSES 10
 #define DIMENSIONS  8192
 //#define DIMENSIONS  32
-#define FEATURE_LEVELS 255
+#define FEATURE_LEVELS 256
 #define FEATURES (28 * 28)
 #define CLASSES 10
+//#define TRAIN_SAMPLES 60000
+#define TRAIN_SAMPLES 2000
 
 
 // Loader
@@ -237,6 +239,19 @@ uint8_t* roll_HV(uint8_t *vector, int D, int shift){
     return t_vector;
 }
 
+// Return the level of a pixel
+uint8_t get_pixel(uint8_t *image, int image_i, int row, int column){
+    int image_addr = image_i*28*28;
+    int pixel_address = image_addr + row*28 + column; 
+    return image[pixel_address];
+}
+
+// Print an HV
+void print_HV (uint8_t *HV, int size){
+    for (int i=0 ; i<size ; i++){
+         printf("%b",get_bit_vector(HV, i));
+    }
+}
 int main()
 {
     int num_images;
@@ -270,6 +285,7 @@ int main()
     }
 
     // Generate the bits of BHVs
+    printf("\n[INFO] Generating BHVs");
     int rolled_i;
     for (uint16_t i=0 ; i<DIMENSIONS ; i++){
         rolled_i = roll_16(i, (i%16));
@@ -279,24 +295,22 @@ int main()
     }
 
     // Print the BHVs
-    printf("\n\n[INFO] BHVs Generated!");
-    printf("\n\ngen_seed1 = ");
-    for (int i=0 ; i<DIMENSIONS ; i++){
-        printf("%b",get_bit_vector(gen_seed1, i));
-    }
-    printf("\n\ngen_seed2 = ");
-    for (int i=0 ; i<DIMENSIONS ; i++){
-        printf("%b",get_bit_vector(gen_seed2, i));
-    }
-    printf("\n\ngen_seed3 = ");
-    for (int i=0 ; i<DIMENSIONS ; i++){
-        printf("%b",get_bit_vector(gen_seed3, i));
-    }
+    printf("\n[INFO] BHVs Generated!\n");
+    // printf("\n\ngen_seed1 = ");
+    // print_HV(gen_seed1, DIMENSIONS)
+    // printf("\n\ngen_seed2 = ");
+    // print_HV(gen_seed2, DIMENSIONS)
+    // printf("\n\ngen_seed3 = ");
+    // print_HV(gen_seed3, DIMENSIONS)
 
+    printf("\npixel = %d\n",get_pixel(images,1512,4,15));
+
+    getchar();
 
     /*#######################################
     ##             Level-ID HVs            ##
     #######################################*/
+    printf("\n[INFO] Generating Level-ID HVs");
 
     uint8_t *level_hvs_bin [FEATURE_LEVELS];
     uint8_t *x_hvs_bin [28];
@@ -312,11 +326,46 @@ int main()
 
     printf("\n\n[INFO] Level_ID HVs Generated!");
 
-    printf("\n\nx_hvs_bin[3] = ");
-    for (int i=0 ; i<DIMENSIONS ; i++){
-        printf("%b",get_bit_vector(x_hvs_bin[3], i));
-    }
+    // printf("\n\nx_hvs_bin[3] = ");
+    // print_HV(level_hvs_bin[get_pixel(images,0,6,21)], DIMENSIONS)
 
+
+    /*#######################################
+    ##           Encoding Images           ##
+    #######################################*/
+
+    printf("\n[INFO] Encoding Images");
+
+    uint16_t encoded [DIMENSIONS];
+    uint8_t *encoded_images [TRAIN_SAMPLES];
+
+    for (int image=0 ; image < TRAIN_SAMPLES ; image++){
+
+        // Initialize integer encoded QHV with 0
+        for (int i=0 ; i<DIMENSIONS ; i++)
+            encoded[i] = 0;
+
+        // Bind and Bundle HVs for each pixel
+        for (int x=0 ; x<28 ; x++){
+            for (int y=0 ; y<28 ; y++){
+                for (int i=0 ; i<DIMENSIONS ; i++){
+                    encoded[i] += get_bit_vector(level_hvs_bin[get_pixel(images, image, y, x)],i) ^ get_bit_vector(x_hvs_bin[x], i) ^ get_bit_vector(y_hvs_bin[y], i);
+                }
+            }
+        }
+
+        // Binarizes the integer encoded HV
+        encoded_images[image] = malloc((DIMENSIONS/8)* sizeof(uint8_t));
+        for (int i=0 ; i<DIMENSIONS ; i++) {
+            set_bit_vector(encoded_images[image], i, (encoded[i]>(FEATURES/2)) ? 1 : 0);
+        }
+    }
+    
+    printf("\n\n[INFO] %d Images Encoded!", TRAIN_SAMPLES);
+    
+
+    printf("\n\nQHV for first image = ");
+    print_HV(encoded_images[1500], DIMENSIONS);
 
     printf("\n");
 
@@ -326,12 +375,15 @@ int main()
 
     free(images);
     free(labels);
-    for (int i = 0; i < FEATURE_LEVELS; i++) {
+    for (int i = 0 ; i < FEATURE_LEVELS ; i++) {
         free(level_hvs_bin[i]);
     }
-    for (int i = 0; i < 28; i++) {
+    for (int i = 0 ; i < 28 ; i++) {
         free(x_hvs_bin[i]);
         free(y_hvs_bin[i]);
+    }
+    for (int i = 0 ; i < TRAIN_SAMPLES ; i++) {
+        free(encoded_images[i]);
     }
     return 0;
 }
